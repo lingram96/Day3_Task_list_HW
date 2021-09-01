bfad4659e40a64c230cb413c2a092bbcd7a1d11d
Lewis <84670468+lingram96@users.noreply.github.com>
2021-09-01 19:09:34 +0100

    Update task_list.py

--- task_list.py
+++ task_list.py
@@ -6,57 +6,57 @@ tasks = [
     { "description": "Walk Dog", "completed": True, "time_taken": 60 },
 ]
 
-# def uncompleted_tasks(list, completed):
-#     uncompleted = []
-#     for task in list:
-#         if task["completed"] == False:
-#             uncompleted.append(task)
-#     return uncompleted
+def uncompleted_tasks(list, completed):
+    uncompleted = []
+    for task in list:
+        if task["completed"] == False:
+            uncompleted.append(task)
+    return uncompleted
 
-# print(uncompleted_tasks(tasks, False))
+print(uncompleted_tasks(tasks, False))
 
-# def completed_tasks(list, completed):
-#     completed = []
-#     for task in list:
-#         if task["completed"] == True:
-#             completed.append(task)
-#     return completed
+def completed_tasks(list, completed):
+    completed = []
+    for task in list:
+        if task["completed"] == True:
+            completed.append(task)
+    return completed
 
-# print(completed_tasks(tasks, True))
+print(completed_tasks(tasks, True))
 
-# def task_descriptions(list):
-#     task_description = []
-#     for task in list:
-#             task_description.append(task["description"])
-#     return task_description
+def task_descriptions(list):
+    task_description = []
+    for task in list:
+            task_description.append(task["description"])
+    return task_description
 
-# print(task_descriptions(tasks))
+print(task_descriptions(tasks))
 
-# def task_time(list):
-#     task_time = []
-#     for task in list:
-#         if task["time_taken"] > 0:
-#             task_time.append(task)
-#     return task_time
+def task_time(list):
+    task_time = []
+    for task in list:
+        if task["time_taken"] > 0:
+            task_time.append(task)
+    return task_time
 
-# print(task_time(tasks))
+print(task_time(tasks))
 
-# def given_description(list, description):
-#     given_description = []
-#     for task in list:
-#         if task["description"] == "Wash Dishes":
-#             given_description.append(task)
-#     return given_description
+def given_description(list, description):
+    given_description = []
+    for task in list:
+        if task["description"] == "Wash Dishes":
+            given_description.append(task)
+    return given_description
 
-# print(given_description(tasks, "Wash Dishes"))
+print(given_description(tasks, "Wash Dishes"))
 
-# tasks[3]["completed"] = True
-# print(tasks)
+tasks[3]["completed"] = True
+print(tasks)
 
-# tasks.append({
-#   "description": "Do Laundry",
-#   "completed": True,
-#   "time_taken": 15
-# })
-# print(tasks)
+tasks.append({
+  "description": "Do Laundry",
+  "completed": True,
+  "time_taken": 15
+})
+print(tasks)
 
